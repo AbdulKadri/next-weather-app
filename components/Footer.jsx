23cc5eee9e6018ffbde49e1bdb4ffe35651cdad9
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-04-14 22:06:04 -0600

    Change padding and link color

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,8 +3,8 @@ import Link from "next/link"
 const Footer = () => {
     return (
         <footer>
-            <p className="text-center bg-gray-400 pb-2">© 2023, Website Made by
-                <Link href="https://github.com/AbdulKadri"> <u>Abdul-Rahime Kadri</u></Link>
+            <p className="text-center bg-gray-400 p-2">© 2023, Website Made by
+                <Link href="https://github.com/AbdulKadri"> <u className="text-red-900">Abdul-Rahime Kadri</u></Link>
             </p>
         </footer>
     )

7159ca7dcfa4c41d29cebedecd71e31dc0514baa
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-04-15 00:44:44 -0600

    Add title for head

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,4 @@
+import Head from "next/head"
 import Hero from "../components/Hero"
 import NotablePlaces from "../components/NotablePlaces"
 import ClimateChange from "../components/ClimateChange"
@@ -5,6 +6,9 @@ import ClimateChange from "../components/ClimateChange"
 const HomePage = () => {
   return (
     <main lang='en'>
+      <Head>
+        <title>Apex Weather</title>
+      </Head>
       <Hero />
       <NotablePlaces />
       <ClimateChange />

ccce60361da5b9a08eeff143110c7d949f800af1
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-04-14 19:24:43 -0600

    Change some styling

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,11 +3,10 @@ import Link from "next/link"
 
 const Nav = () => {
     return (
-        <nav className="w-full flex justify-around sticky top-0 border border-solid border-red-400">
+        <nav className="w-full flex justify-between items-center sticky top-0 bg-background py-3 px-6">
             <Link href={'/'}><h2>Weather App</h2></Link>
-
-            <div className="w-3/6 border border-solid border-blue-400 rounded full">
-                <CitySearch className="border border-solid border-red-400" />
+            <div className="flex justify-end w-3/6">
+                <CitySearch />
             </div>
         </nav>
     )

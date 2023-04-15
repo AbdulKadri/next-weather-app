a3ebad478e952823ef17b0fb76012d5ab842d56e
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-04-15 00:45:36 -0600

    Change error handling, styling & add author credit

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react';
 import { FaMapPin } from 'react-icons/fa'
 import CitySearch from '../components/CitySearch'
+import Link from 'next/link'
 
 const Hero = () => {
-    const [searchError, setSearchError] = useState(false);
+    const [error, setError] = useState(false);
+
+    const handleError = (hasError) => {
+        setError(hasError);
+    };
 
     return (
         <main lang='en'>
-            <div className="min-h-[80vh] w-screen
+            <div className="relative min-h-[80vh] w-screen
        bg-temp-image bg-cover bg-center bg-no-repeat shadow-cover
        grid grid-cols-2 place-content-center place-items-stretch">
                 <div className="flex flex-col justify-center items-center">
@@ -16,12 +21,25 @@ const Hero = () => {
                     </h1>
                     <p className="text-2xl text-white w-4/5 text-center">Discover the current Weather, Air Pollution index & more in your city</p>
 
-                    <CitySearch onError={setSearchError} searchError={searchError} />
+                    <div className="flex flex-col items-center gap-1 w-2/5">
+                        <CitySearch onError={handleError} className="w-full" />
+                        {error && (
+                            <p className="text-red-600 p-1 text-lg w-fit bg-gray-400 rounded-md">
+                                Please enter a valid city name.
+                            </p>
+                        )}
+                    </div>
 
                     <div className="flex mt-2">
                         <FaMapPin color="#68EDC6" className="h-6 w-6" />
                         <p className="text-xl text-white">Check your location</p>
                     </div>
+
+                    <p className="absolute bottom-0 left-0 text-white font-bold p-3">
+                        Image by
+                        {' '}
+                        <Link href="https://unsplash.com/@benobro"><u>ben o'bro</u></Link>
+                    </p>
                 </div>
             </div>
         </main>

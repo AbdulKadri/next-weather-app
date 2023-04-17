ffc0957b5f2a4db8d7b4db56dd2b6825d5fae794
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-04-17 17:20:13 -0600

    remove unecessary classnames

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -13,8 +13,8 @@ const Nav = () => {
     return (
         <nav className="w-full flex justify-center sticky top-0 bg-gray-400 z-10">
             <div className="w-2/5 flex justify-between items-center">
-                <Link href={'/'} className="">
-                    <Image src={'/Apex-Weather-Logo.svg'} alt={'Apex Weather logo'} width={150} height={150} className="" />
+                <Link href={'/'} >
+                    <Image src={'/Apex-Weather-Logo.svg'} alt={'Apex Weather logo'} width={150} height={150} />
                 </Link>
                 <div className="flex justify-end">
                     <div className="flex flex-col gap-1">

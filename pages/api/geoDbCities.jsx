5b97bedf80181e8212d33df161023679c397e1a6
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-04-17 17:19:34 -0600

    Add geoDbCity Api

---
+++ pages/api/geoDbCities.jsx
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'e992b32dbfmshfc7b9c5b47e906fp15fd81jsn188bdb26782d',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';

8107c5b1628699d3bdda79cb94c4c288f49bca05
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-04-19 19:38:58 -0600

    add initial display of all information

--- components/Forecast.jsx
+++ components/Forecast.jsx
@@ -7,23 +7,53 @@ const Forecast = ({ data }) => {
     const forecastDays = WEEK_DAYS.slice(dayInAWeek, WEEK_DAYS.length).concat(WEEK_DAYS.slice(0, dayInAWeek));
 
     return (
-        <div className="h-full">
-            <h1 className="text-white">Daily</h1>
-            <Accordion allowZeroExpanded className="flex flex-wrap gap-10 text-white">
+        <div className="flex flex-col w-4/5 h-full my-2">
+            <h1 className="text-3xl">Daily</h1>
+            <Accordion allowZeroExpanded className="flex flex-col gap-4">
                 {data.list.splice(0, 7).map((day, index) => (
                     <AccordionItem key={index}>
                         <AccordionItemHeading>
                             <AccordionItemButton>
-                                <div className="flex flex-col">
-                                    <img src={`http://openweathermap.org/img/w/${day.weather[0].icon}.png`} alt="weather icon" />
-                                    <p className="text-lg">{forecastDays[index]}</p>
-                                    <p className="text-5xl">{Math.round(day.main.temp)}<span>&#176;C</span></p>
-                                    <p>{day.weather[0].description}</p>
+                                <div className="flex justify-between items-center p-4 bg-black/80 text-white rounded-md">
+                                    <div className="flex gap-2">
+                                        <img src={`http://openweathermap.org/img/w/${day.weather[0].icon}.png`} alt="weather icon" />
+                                        <p className="text-lg">{forecastDays[index]}</p>
+                                        <p className="text-5xl">{Math.round(day.main.temp)}<span>&#176;C</span></p>
+                                    </div>
+                                    <div>
+                                        <p>{day.weather[0].description}</p>
+                                        <p>{Math.round(day.main.temp_min)}&#176;C / {Math.round(day.main.temp_max)}&#176;C</p>
+                                    </div>
                                 </div>
                             </AccordionItemButton>
                         </AccordionItemHeading>
                         <AccordionItemPanel>
-                            Opened
+                            <div className="flex flex-col gap-1">
+                                <div className="flex justify-around text-2xl bg-gray-400/80">
+                                    <h2>Pressure:</h2>
+                                    <p>{day.main.pressure}hPa</p>
+                                </div>
+                                <div className="flex justify-around text-2xl">
+                                    <h2>Humidity:</h2>
+                                    <p>{day.main.humidity}%</p>
+                                </div>
+                                <div className="flex justify-around text-2xl bg-gray-400/80">
+                                    <h2>Pressure:</h2>
+                                    <p>{day.clouds.all}</p>
+                                </div>
+                                <div className="flex justify-around text-2xl">
+                                    <h2>Wind Speed:</h2>
+                                    <p>{day.wind.speed}m/s</p>
+                                </div>
+                                <div className="flex justify-around text-2xl bg-gray-400/80">
+                                    <h2>Sea Level:</h2>
+                                    <p>{day.main.sea_level}m</p>
+                                </div>
+                                <div className="flex justify-around text-2xl">
+                                    <h2>Feels Like:</h2>
+                                    <p>{Math.round(day.main.feels_like)}&#176;C</p>
+                                </div>
+                            </div>
                         </AccordionItemPanel>
                     </AccordionItem>
                 ))}

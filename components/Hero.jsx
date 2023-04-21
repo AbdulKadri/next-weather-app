dfcda87ce27788e6678860ea27282fadc59c7e03
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-04-20 23:47:47 -0600

    Add loading component and current location feature

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,14 +1,47 @@
 import { useState } from 'react';
 import { FaMapPin } from 'react-icons/fa'
 import CitySearch from '../components/CitySearch'
-import Link from 'next/link'
+import { useRouter } from 'next/router'
+import Loading from '../components/Loading'
 
 const Hero = () => {
     const [error, setError] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const router = useRouter();
 
     const handleError = (hasError) => {
         setError(hasError);
     };
+
+    const handleGetLocation = async () => {
+        setIsLoading(true);
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(async (position) => {
+                const cityName = await getCityName(position.coords.latitude, position.coords.longitude);
+
+                if (cityName) {
+                    router.push(`/${cityName}?lat=${position.coords.latitude}&lon=${position.coords.longitude}`);
+                } else {
+                    alert('Failed to fetch city name.');
+                }
+            });
+        } else {
+            alert('Geolocation is not supported by this browser.');
+        }
+    };
+
+    const getCityName = async (latitude, longitude) => {
+        try {
+            const response = await fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}&zoom=10&addressdetails=1`);
+            const data = await response.json();
+            return data.address.city || data.address.town || data.address.village;
+        } catch (error) {
+            console.error('Error fetching city name:', error);
+            return null;
+        }
+    };
+
+    if (isLoading) return (<Loading />);
 
     return (
         <main lang='en'>
@@ -30,15 +63,15 @@ const Hero = () => {
                         )}
                     </div>
 
-                    <div className="flex mt-2">
+                    <button onClick={handleGetLocation} className="flex mt-2">
                         <FaMapPin color="#68EDC6" className="h-6 w-6" />
                         <p className="text-xl text-white">Check your location</p>
-                    </div>
+                    </button>
 
                     <p className="absolute bottom-0 left-0 text-white font-bold p-3">
-                        Image by
+                        Photo by
                         {' '}
-                        <Link href="https://unsplash.com/@benobro"><u>ben o'bro</u></Link>
+                        <a href="https://unsplash.com/@benobro" target='_blank' rel="noopener noreferrer"><u>ben o'bro</u></a>
                     </p>
                 </div>
             </div>

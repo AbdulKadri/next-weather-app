029976e8bebbc84b6a8b106773ea1d7e25db57a4
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-04-20 23:19:12 -0600

    Add units and fix some styling

--- components/Forecast.jsx
+++ components/Forecast.jsx
@@ -15,9 +15,9 @@ const Forecast = ({ data }) => {
                         <AccordionItemHeading>
                             <AccordionItemButton>
                                 <div className="flex justify-between items-center p-4 bg-black/80 text-white rounded-md">
-                                    <div className="flex gap-2">
+                                    <div className="flex items-center gap-2">
                                         <img src={`http://openweathermap.org/img/w/${day.weather[0].icon}.png`} alt="weather icon" />
-                                        <p className="text-lg">{forecastDays[index]}</p>
+                                        <p className="text-3xl">{forecastDays[index]}</p>
                                         <p className="text-5xl">{Math.round(day.main.temp)}<span>&#176;C</span></p>
                                     </div>
                                     <div>
@@ -31,7 +31,7 @@ const Forecast = ({ data }) => {
                             <div className="flex flex-col gap-1">
                                 <div className="flex justify-around text-2xl bg-gray-400/80">
                                     <h2>Pressure:</h2>
-                                    <p>{day.main.pressure}hPa</p>
+                                    <p>{day.main.pressure} hPa</p>
                                 </div>
                                 <div className="flex justify-around text-2xl">
                                     <h2>Humidity:</h2>
@@ -39,7 +39,7 @@ const Forecast = ({ data }) => {
                                 </div>
                                 <div className="flex justify-around text-2xl bg-gray-400/80">
                                     <h2>Pressure:</h2>
-                                    <p>{day.clouds.all}</p>
+                                    <p>{day.clouds.all}%</p>
                                 </div>
                                 <div className="flex justify-around text-2xl">
                                     <h2>Wind Speed:</h2>

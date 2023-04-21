ff0c24e3b788d72b72e7739942deaebc2d7ebd7d
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-04-20 23:46:00 -0600

    Add cityimageUsername to data

--- pages/api/unsplash.jsx
+++ pages/api/unsplash.jsx
@@ -7,13 +7,14 @@ const getImageData = async (req, res) => {
         const response = await axios.get(`https://api.unsplash.com/search/photos?query=${city}&client_id=${process.env.UNSPLASH_API_KEY}`);
 
         const cityImageUrl = response.data.results[0]?.urls?.regular;
+        const cityImageUser = response.data.results[0]?.user?.username;
 
         if (!cityImageUrl) {
             res.status(404).json({ message: 'Image not found' });
             return;
         }
 
-        res.status(200).json({ cityImageUrl });
+        res.status(200).json({ cityImageUrl, cityImageUser });
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'An error occurred' });

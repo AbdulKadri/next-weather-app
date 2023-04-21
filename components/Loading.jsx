6bfc27711f800b90a74462289b3d2c60071c7430
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-04-20 23:45:44 -0600

    Add loading component

---
+++ components/Loading.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+    return (
+        <div className="flex justify-center items-center bg-black/80 h-screen text-white text-3xl">
+            Loading...
+        </div>
+    )
+}
+
+export default Loading

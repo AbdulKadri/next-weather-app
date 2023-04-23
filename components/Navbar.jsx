a810b26e59d88b5233b792985df502efc8bc093a
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-04-23 06:43:36 -0600

    Change size of search bar and spacing

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,13 +12,13 @@ const Nav = () => {
 
     return (
         <nav className="w-full flex justify-center sticky top-0 bg-gray-400 z-10">
-            <div className="w-2/5 flex justify-between items-center">
+            <div className="w-4/5 flex justify-between items-center">
                 <Link href={'/'} >
                     <Image src={'/Apex-Weather-Logo.svg'} alt={'Apex Weather logo'} width={150} height={150} />
                 </Link>
                 <div className="flex justify-end">
                     <div className="flex flex-col gap-1">
-                        <CitySearch onError={handleError} className="w-full" />
+                        <CitySearch onError={handleError} className="w-[20rem]" />
                         {error && (
                             <div className="text-red-600 text-lg text-center">
                                 Please enter a valid city name.

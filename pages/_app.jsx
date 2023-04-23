8d02c229bac294d69dd9ff6f89b2a2f665e8facc
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-04-23 06:43:10 -0600

    add leaflet css

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,7 @@
 import Layout from '@/components/Layout'
 import '@/styles/globals.css'
 import { Quicksand } from 'next/font/google'
+import 'leaflet/dist/leaflet.css';
 
 const quicksand = Quicksand({
   weights: [400, 500, 600, 700],

f62044a7c0faaf8e35b8c8af1b382dceea89265f
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-04-23 06:40:45 -0600

    Add fetch timeout utility

---
+++ utils/fetchWithTimeout.jsx
@@ -0,0 +1,8 @@
+const fetchWithTimeout = (url, options, timeout = 5000) => {
+    return Promise.race([
+        fetch(url, options),
+        new Promise((_, reject) =>
+            setTimeout(() => reject(new Error('Request timed out')), timeout)
+        ),
+    ]);
+};

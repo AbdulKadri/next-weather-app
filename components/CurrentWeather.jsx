8d20c10f8fe3fff800945df387b0d65072da3a39
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-04-24 16:28:51 -0600

    Change icons, styling and remove sea level

--- components/CurrentWeather.jsx
+++ components/CurrentWeather.jsx
@@ -92,29 +92,29 @@ const CurrentWeather = ({ data, cityImageUrl, cityImageUsernameData }) => {
         <div className='flex w-full h-[90vh] relative'>
             <div className='basis-1/2'>
                 <Image src={cityImage} alt='city image' width={1920} height={1080} className='w-full h-full' priority={true} />
-                <p className='absolute bottom-0 left-0 text-white font-bold p-3'>
+                <p className='absolute bottom-0 left-0 text-white font-bold p-3 bg-black/30 rounded-lg'>
                     Photo by <a href={`http://unsplash.com/${cityImageUsername}`} target='_blank' rel="noopener noreferrer"><u>{cityImageUsername}</u></a>
                 </p>
             </div>
             <div className='basis-1/2 flex flex-col items-center bg-black/80 text-white'>
                 <div className='m-2'>
                     <h1 className="text-5xl m-2 text-center text-white bold">{cityName} {countryName}</h1>
-                    <p className="text-center text-red-400 text-3xl">At {currentDate}, {currentTime}</p>
+                    <p className="text-center text-primary text-3xl">At {currentDate}, {currentTime}</p>
                 </div>
-                <div className='flex items-center justify-center w-full px-5'>
+                <div className='flex items-center justify-center w-full'>
                     <div className='flex flex-col items-center w-full'>
-                        <div className='flex items-center mt-2'>
-                            <img src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`} width={100} height={100} className='mr-3' />
-                            <p className='text-center text-4xl font-bold'>{currentTemp}<span>&#176;C</span></p>
+                        <div className='flex items-center gap-2 mt-2'>
+                            <img src={`icons/${data.weather[0].icon}.png`} width={80} height={80} />
+                            <p className='text-center text-5xl font-bold'>{currentTemp}<span>&#176;C</span></p>
                         </div>
                         <div className='flex flex-col items-center w-full mt-2'>
                             <p className='text-center text-lg'>{data.weather[0].description}</p>
                             <p className='text-center mb-2 text-2xl'>Feels Like: {currentFeelsLike}<span>&#176;C</span></p>
-                            <div className='flex justify-between w-full'>
+                            <div className='flex justify-evenly w-full px-5'>
                                 <p className='text-lg bg-gradient-to-r from-yellow-400 to-orange-500 text-transparent bg-clip-text'>Sunrise: {sunrise}</p>
                                 <p className='text-lg bg-gradient-to-r from-blue-300 to-blue-500 text-transparent bg-clip-text'>Sunset: {sunset}</p>
                             </div>
-                            <div className='flex justify-between w-full mt-2'>
+                            <div className='flex justify-evenly w-full mt-2 px-5'>
                                 <div className='text-center'>
                                     <p className='text-md'>Pressure</p>
                                     <p className='text-lg font-bold'>{data.main.pressure}hPa</p>
@@ -122,10 +122,6 @@ const CurrentWeather = ({ data, cityImageUrl, cityImageUsernameData }) => {
                                 <div className='text-center'>
                                     <p className='text-md'>Humidity</p>
                                     <p className='text-lg font-bold'>{data.main.humidity}%</p>
-                                </div>
-                                <div className='text-center'>
-                                    <p className='text-md'>Sea Level</p>
-                                    <p className='text-lg font-bold'>{data.main.sea_level}m</p>
                                 </div>
                                 <div className='text-center'>
                                     <p className='text-md'>Cloudiness</p>

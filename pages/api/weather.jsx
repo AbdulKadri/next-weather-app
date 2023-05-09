36bb0ab42fdb10a4aed094a8da7da60d7aaff3f9
Abdul-Rahime Kadri <abdul.r.kadri@gmail.com>
2023-05-09 17:11:39 -0600

    Add AQ & change from waterfall to parallel promise

--- pages/api/weather.jsx
+++ pages/api/weather.jsx
@@ -5,19 +5,22 @@ const getWeatherData = async (req, res) => {
     const { lat, lon, unit } = req.query;
 
     try {
-        const currentWeatherResponse = await axios.get(`${process.env.NEXT_PUBLIC_WEATHER_URL}/weather?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}&units=${unit}`);
+        const [currentWeatherResponse, forecastResponse, airQualityResponse] = await Promise.all([
+            axios.get(`${process.env.NEXT_PUBLIC_WEATHER_URL}/weather?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}&units=${unit}`),
+            axios.get(`https://pro.openweathermap.org/data/2.5/forecast/climate?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}&units=${unit}`),
+            axios.get(`http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}`)
+        ]);
+
         const currentWeatherData = currentWeatherResponse.data;
-
-        // const forecastResponse = await axios.get(`${process.env.WEATHER_URL}/forecast?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}&units=metric`);
-        const forecastResponse = await axios.get(`https://pro.openweathermap.org/data/2.5/forecast/climate?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}&units=${unit}`);
         const forecastData = forecastResponse.data;
+        const airQualityData = airQualityResponse.data;
 
-        if (!currentWeatherData || !forecastData) {
+        if (!currentWeatherData || !forecastData || !airQualityData) {
             res.status(404).json({ message: 'Weather data not found' });
             return;
         }
 
-        res.status(200).json({ currentWeatherData, forecastData });
+        res.status(200).json({ currentWeatherData, forecastData, airQualityData });
     } catch (error) {
         console.error(error);
         if (error.response && error.response.status === 404) {
